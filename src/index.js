12502ac1d083f06268e0512d23782962ac829d5d
runrunryosuke <runrunryosuke0425@gmail.com>
2021-03-31 19:04:28 +0900

    finish

--- src/index.js
+++ src/index.js
@@ -1,22 +1,48 @@
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
 /* ------------------------------
 Square
 ------------------------------ */
-const Square = (props) => {
-  return <button className="square">{props.value}</button>;
+const Square = ({ value, onClick }) => {
+  return (
+    <button className="square" onClick={() => onClick()}>
+      {value}
+    </button>
+  );
 };
 
 /* ------------------------------
 Board
 ------------------------------ */
 const Board = () => {
-  const renderSquare = (i) => {
-    return <Square value={i} />;
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const handleClick = (i) => {
+    const _squares = squares.slice();
+
+    if (calculateWinner(_squares) || _squares[i]) {
+      return;
+    }
+    _squares[i] = xIsNext ? "X" : "O";
+    setSquares(_squares);
+    setXIsNext(!xIsNext);
   };
-  const status = "Next player: X";
+
+  const renderSquare = (i) => {
+    return <Square value={squares[i]} onClick={() => handleClick(i)} />;
+  };
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+  
   return (
     <div>
       <div className="status">{status}</div>
@@ -55,6 +81,26 @@ const Game = () => {
     </div>
   );
 };
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
 
 // ========================================
 
